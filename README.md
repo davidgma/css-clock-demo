ea1b429eebb1fd676744c283978b4ff29df12312
davidgma <35199772+davidgma@users.noreply.github.com>
2023-04-29 13:24:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# css-clock-demo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/css-clock-demo)
---
+++ docs/index.ts
@@ -0,0 +1,62 @@
+
+onload = (event) => {
+  console.log('page loaded');
+  setHands();
+
+  console.log("seconds to next whole minute: " + millisecondsToNextMinute() / 1000);
+  setTimeout(setHands, millisecondsToNextMinute());
+  setTimeout(updateClockRegularly, millisecondsToNextMinute());
+};
+
+
+function setHands() {
+  /* to set current time */
+  // console.log("in setHands");
+  const time = new Date();
+  const hour = -3600 * (time.getHours() % 12);
+  const mins = -60 * time.getMinutes();
+  const secs = -1 * time.getSeconds();
+
+  // Get the root element
+  let r = document.querySelector(":root");
+  reset_animation();
+  if (r != null && r instanceof HTMLElement) {
+    r.style.setProperty("--_dm", `${mins}s`);
+    r.style.setProperty("--_dh", `${hour + mins}s`);
+    r.style.setProperty("--_ds", `${secs}s`);
+
+  }
+
+}
+
+function millisecondsToNextMinute() {
+  // calculate next whole minute
+  const now = new Date();
+  const lastMinute = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    now.getHours(),
+    now.getMinutes()
+  );
+  const nextMinute = lastMinute.valueOf() + 60 * 1000;
+  return (nextMinute - now.valueOf());
+
+}
+
+function updateClockRegularly() {
+  // console.log("in updateClockRegularly");
+  setInterval(setHands, 60 * 1000);
+}
+
+function reset_animation() {
+  let elements = document.getElementsByClassName("arm");
+  for (let element of elements) {
+    if (element instanceof HTMLElement) {
+      element.style.animation = "none";
+      element.offsetHeight; /* trigger reflow */
+      element.style.animation = "";
+    }
+
+  }
+}
